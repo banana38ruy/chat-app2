ab0cf344d62813e1fc24112348abb9eab6bba137
banana38ruy <banana38ruy@yahoo.co.jp>
2020-12-18 18:24:35 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,34 @@
-class MessagesController < ApplicationController
+class MessagesController < ApplicationController 
   def index
+    @message = Message.new
+    @room    = Room.find(params[:room_id])
+  #roomはmessageにネストされているため
+  #パスにroom:idが含まれているそれをfindするため
+  #Room.find(params[:room_id])という表記になる
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+       redirect_to room_messages_path(@room)
+       #メッセージの保存に成功した場合、redirect_toメソッドを用いて
+       #messagesコントローラーのindexアクションに再度リクエストを送信し
+       #インスタンス変数を生成する
+    else
+     render :index
+     #メッセージの保存に失敗した場合、renderメソッドを用いてindexアクションの
+     #index.html.erbを表示するように指定しているこのとき、indexアクションの
+     #インスタンス変数はそのままindex.html.erbに渡され、同じページに戻る
+    end
+  end
+
+ private
+
+ def message_params
+ params.require(:message).permit(:content).merge(user_id: current_user.id)
+ end
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
-  #新規チャットルームの作成で動くアクションは「new」と「create」のみ
+  resources :rooms, only: [:new, :create] do
+     resources :messages, only: [:index, :create]
+  end
+ #新規チャットルームの作成で動くアクションは「new」と「create」のみ
+ #メッセージを投稿する際には、どのルームで投稿されたメッセージなのかを
+ #パスから判断できるようにしたいので、ルーティングのネストを利用する
+ #こうすることによってroomsが親、messageが子という親子関係になるので
+ #チャットルームに属しているメッセージという意味になる
+  
 end
