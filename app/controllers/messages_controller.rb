39f74650470ffc55dbcb525641cfca76aaef15ae
banana38ruy <banana38ruy@yahoo.co.jp>
2020-12-18 20:16:14 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,13 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room    = Room.find(params[:room_id])
+    #チャットルームに紐付いている全てのメッセージを@messagesと定義する
+    #一覧画面で表示するメッセージ情報にはユーザー情報も紐付いて表示される
+    #この場合、メッセージに紐付くユーザー情報の取得に、メッセージの数と
+    #同じ回数のアクセスが必要になるのでN＋１問題が発生する
+    #includes(:user)と記述することにより、ユーザー情報を
+    #一度のアクセスでまとめて取得できる
+    @messages = @room.messages.includes(:user)
   #roomはmessageにネストされているため
   #パスにroom:idが含まれているそれをfindするため
   #Room.find(params[:room_id])という表記になる
@@ -16,6 +23,12 @@ class MessagesController < ApplicationController
        #messagesコントローラーのindexアクションに再度リクエストを送信し
        #インスタンス変数を生成する
     else
+     @messages = @room.messages.includes(:user)
+     #また投稿に失敗したときの処理にも、同様に@messagesを定義した
+     #renderを用いることで、投稿に失敗した@messagesの情報を保持しつつ
+     #index.html.erbを参照することができる
+     #しかしながらそのときに@messagesが定義されていないとエラーになってしまう
+     #そこでindexアクションと同様に@messagesを定義する必要がある
      render :index
      #メッセージの保存に失敗した場合、renderメソッドを用いてindexアクションの
      #index.html.erbを表示するように指定しているこのとき、indexアクションの
